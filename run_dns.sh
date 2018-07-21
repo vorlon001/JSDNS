9d27372914b7ce2f4d668981f5e3f5efc1c8f7de
vorlon001 <vorlon001@users.noreply.github.com>
2018-07-21 23:34:02 +0500

    Update run_dns.sh

--- run_dns.sh
+++ run_dns.sh
@@ -33,7 +33,7 @@ mkdir /docker/host/jsdns_$1_$2/data
 
 cp -r jsdns /docker/host/jsdns_$1_$2/
 
-docker run --name redis_$1_$2 \
+docker run --name jsdns_$1_$2 \
 -v /docker/host/jsdns_$1_$2/data:/var/lib/redis \
 -v /docker/host/jsdns_$1_$2/log:/opt/logs  \
 --net net_172_19 --ip $2 \
